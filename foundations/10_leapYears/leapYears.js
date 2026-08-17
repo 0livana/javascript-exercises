563ea8bc0e8aff6fb82361b8813f47f9703a2497
Oliva Nana <olivanascript@gmail.com>
2026-08-17 07:13:06 +0100

    feat: add the leaYear function

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,6 +1,13 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if(year % 4 === 0 && (year % 400 === 0 || !(year % 100 === 0) )) {
+        return `${year} is a leap year.`
+    } else {
+        return `${year} is not a leap year.`
+    }
 };
+
+console.log(leapYears(2024))
+console.log(leapYears(2026))
 
 // Do not edit below this line
 module.exports = leapYears;
