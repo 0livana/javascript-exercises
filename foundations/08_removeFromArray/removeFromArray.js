11ca82d4ef2e5d1d4e551cad853fa0c773fe1299
Oliva Nana <olivanascript@gmail.com>
2026-08-17 05:55:31 +0100

    feat: create the removeFromArray function

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...arg) {
+    const newArr = [];
+    arr.forEach(item => {
+        if(!arg.includes(item)){
+            newArr.push(item)
+        }
+    })
+    return newArr;
 };
-
+ 
+const arrays = removeFromArray([3, 5, 2, 4], 5)
+console.log(arrays)
 // Do not edit below this line
 module.exports = removeFromArray;
