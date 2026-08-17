ae4df146dc843da128cb5d5b52e1eba7d0706554
Oliva Nana <olivanascript@gmail.com>
2026-08-17 09:53:29 +0100

    feat: add the temperature converter functions

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,9 +1,24 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahr) {
+  const result = (fahr - 32) * 5/9;
+  if(Number.isInteger(result)){
+    return result + " degree celsius."
+  } else {
+    return result.toFixed(1) + " degree celsius."
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  const result = (celsius * 9/5) +32;
+  if(Number.isInteger(result)){
+    return result + " degree fahrenheit."
+  } else {
+    return result.toFixed(1) + " degree fahrenheit."
+  }
 };
 
+const checkCelsius = convertToCelsius(100)
+console.log(checkCelsius)
+console.log(convertToFahrenheit(100))
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
