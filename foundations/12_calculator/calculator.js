fbc8139e5563f173dcd4e3678311a3c04432fa37
Oliva Nana <olivanascript@gmail.com>
2026-08-20 13:33:01 +0100

    feat: complete the calculator exercise

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((acc, currV) =>  acc + currV, 0)
+  
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let total = 1;
+  for(const value of arr){
+    total *= value
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return base ** exponent
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let result = 1;
+	for(let i = 1; i <= num; i++){
+    result *= i;
+  }
+  return result
 };
 
 // Do not edit below this line
@@ -31,3 +41,10 @@ module.exports = {
   power,
   factorial
 };
+
+console.log(add(4, 7));
+console.log(subtract(20, 7));
+console.log(sum([2, 5, 7, 1]));
+console.log(multiply([2, 5, 7, 1]));
+console.log(power(4, 2));
+console.log(factorial(5));
