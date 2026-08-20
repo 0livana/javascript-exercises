46cc3af8d7ca20edc0ad8dd427a436cd0420f3b3
Oliva Nana <olivanascript@gmail.com>
2026-08-20 15:26:59 +0100

    feat: complete the get book title exercise

--- foundations/15_getTheTitles/getTheTitles.js
+++ foundations/15_getTheTitles/getTheTitles.js
@@ -1,6 +1,23 @@
-const getTheTitles = function() {
+const books = [
+  {
+    title: 'Robert Kiyosaki',
+    author: 'Rich Dad Poor Dad'
+  },
+  {
+    title: 'William Shakespare',
+    author: 'Book2'
+  }
+]
 
+const getTheTitles = function(books) {
+    let titles = [];
+    for(const book of books){
+        titles.push(book.title)
+    }
+    return titles
 };
+
+console.log(getTheTitles(books))
 
 // Do not edit below this line
 module.exports = getTheTitles;
