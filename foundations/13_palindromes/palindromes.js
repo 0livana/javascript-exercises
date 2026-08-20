ed213bdd9be88996f841eaf893c41d6866a2d392
Oliva Nana <olivanascript@gmail.com>
2026-08-20 14:07:58 +0100

    feat: complete the palindrome exercise

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,17 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const reversed = str.toLowerCase().split("").reverse().join("");
+    if(str.toLowerCase() === reversed){
+        return `${str} is a Palindrome`; 
+    } else {
+        return `${str} is not a Palindrome`;
+    }
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
+
+
+
+console.log(palindromes("Hello"));
+console.log(palindromes("Racecar"));
