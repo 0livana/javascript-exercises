d3f35cd717f3baf776dcb0e79e26aad7e0970337
Oliva Nana <olivanascript@gmail.com>
2026-08-20 15:08:17 +0100

    feat: complete the fibonacci exercise

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,6 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    let a = 0;
+    let b = 1;
 
+    for (let i = 0; i < n; i++) {
+        const next = a + b;
+        a = b;
+        b = next;
+    }
+
+    return a;
 };
+
+console.log(fibonacci(2))
 
 // Do not edit below this line
 module.exports = fibonacci;
