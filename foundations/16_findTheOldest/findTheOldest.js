58a49e76e483ae2fe71b91d6ad1208fe865db20b
Oliva Nana <olivanascript@gmail.com>
2026-08-20 15:47:55 +0100

    complete the getTheOldest person exercise

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,53 @@
-const findTheOldest = function() {
+const data = [
+    {
+        name: "John",
+        DOB: 1990,
+        DOD: 2010
+    },
+    {
+        name: "Brian",
+        DOB: 1950,
+        DOD: 2015
+    },
+    {
+        name: "Cartel",
+        DOB: 1974,
+        DOD: 2018
+    },
+    {
+        name: "Brian",
+        DOB: 1990,
+        DOD: 2024
+    },
+    {
+        name: "Brian",
+        DOB: 1969,
+        DOD: 2020
+    }
+    
+]
 
+const findTheOldest = function(people) {
+    let oldest = people[0];
+
+    for (const person of people) {
+        const birthYear = person.DOB;
+        const deathYear = person.DOD || new Date().getFullYear();
+
+        const age = deathYear - birthYear;
+        const oldestAge =
+            (oldest.DOD || new Date().getFullYear()) - oldest.DOB;
+
+        if (age > oldestAge) {
+            oldest = person;
+        }
+    }
+
+    return oldest;
 };
+
+
+console.log(findTheOldest(data));
 
 // Do not edit below this line
 module.exports = findTheOldest;
